ef762827386a93255c471cf2b1bc02b59db654af
Lautaro Lamaita <lautarolamaita@gmail.com>
2026-03-10 00:09:10 -0300

    feat: add POST create task endpoint

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -3,17 +3,17 @@ import createTask from "../utils/createTask.js";
 
 /**
  * GET /api/tasks
- * Trae todas las tareas
+ * Returns all tasks
  */
-export const getAllTasks = (req, res) => {
+const getAllTasks = (req, res) => {
   res.status(200).json(tasks);
 };
 
 /**
  * GET /api/tasks/:id
- * Obtiene una tarea por id
+ * Returns a task by id
  */
-export const getTaskById = (req, res) => {
+const getTaskById = (req, res) => {
   const id = Number(req.params.id);
   const task = tasks.find((task) => task.id === id);
 
@@ -25,3 +25,41 @@ export const getTaskById = (req, res) => {
 
   res.status(200).json(task);
 };
+
+/**
+ * Post /api/tasks/
+ * Creates a new task
+ */
+const addTask = (req, res) => {
+  const { title, description } = req.body;
+
+  if (!title || title.trim() === "") {
+    return res.status(400).json({
+      error: "The title entered must not be empty!",
+    });
+  }
+
+  if (!description || description.trim() === "") {
+    return res.status(400).json({
+      error: "The description entered must not be empty",
+    });
+  }
+
+  const taskExists = tasks.some(
+    (task) =>
+      task.title.toLowerCase() === title.trim().toLowerCase() &&
+      task.description.toLowerCase() === description.trim().toLowerCase(),
+  );
+
+  if (taskExists) {
+    return res.status(409).json({
+      error: "Task already exists",
+    });
+  }
+
+  const newTask = createTask(title, description);
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+};
+
+export { getAllTasks, getTaskById, addTask };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllTasks, getTaskById } from "../controllers/taskController.js";
+import {
+  addTask,
+  getAllTasks,
+  getTaskById,
+} from "../controllers/taskController.js";
+import createTask from "../utils/createTask.js";
 
 const router = express.Router();
 
 router.get("/", getAllTasks);
 router.get("/:id", getTaskById);
+router.post("/", addTask);
 
 export default router;
